d72eab94e71b18657b10af574e2b684afea6d9ac
Higor <higor.ruan456@gmail.com>
2024-03-16 15:58:57 -0300

    creates create-tables migration, changes knex types

---
+++ db/migrations/20240316002617_create-tables.ts
@@ -0,0 +1,44 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id').index()
+    table.text('username').notNullable()
+    table.text('email').notNullable()
+    table.text('password').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id').index()
+    table.text('name').notNullable()
+    table.text('description')
+    table.timestamp('meal_time').defaultTo(knex.fn.now()).notNullable()
+    table.boolean('diet_meal').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+
+  await knex.schema.createTable('metrics', (table) => {
+    table.uuid('id').primary()
+    table.uuid('session_id').index()
+    table.integer('meal_amount').notNullable()
+    table.integer('diet_amount').notNullable()
+    table.integer('not_diet_amount').notNullable()
+    table.integer('diet_sequence').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+  await knex.schema.dropTable('meals')
+  await knex.schema.dropTable('metrics')
+}
+
--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -3,11 +3,33 @@ import  { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    /* example: {
+    users: {
       id: string
+      session_id: string
       title: string
       amount: number
       created_at: string
-    } */
+      updated_at: string
+    },
+    meals: {
+      id: string
+      session_id: string
+      name: string
+      description: string | null
+      meal_time: string
+      diet_meal: boolean
+      created_at: string
+      updated_at: string
+    },
+    metrics: {
+      id: string
+      session_id: string
+      meal_amount: number
+      diet_amount: number
+      not_diet_amount: number
+      diet_sequence: number
+      created_at: string
+      updated_at: string
+    }
   }
 }
